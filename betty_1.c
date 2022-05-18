a6d348aebd81229da5dd50381c3494bba4b7f8a5
GMercury <kingorigithinji@gmail.com>
2022-05-19 00:03:17 +0300

    0x16 simple shell

---
+++ betty_1.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+#include<readline/readline.h>
+#include<readline/history.h>
+  
+#define MAXCOM 1000 // max number of letters to be supported
+#define MAXLIST 100 // max number of commands to be supported// Function to take input
+int takeInput(char* str)
+{
+    char* buf;
+  
+    buf = readline("\n>>> ");
+    if (strlen(buf) != 0) {
+        add_history(buf);
+        strcpy(str, buf);
+        return 0;
+    } else {
+        return 1;
+    }
+}
